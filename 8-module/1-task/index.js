21671c95cf976105a30e98a91ab7070f3c621ff1
Mogvay-web <mogvay73@gmail.com>
2024-07-31 00:03:44 +0300

    8.1 task solution

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,4 +1,4 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class CartIcon {
   constructor() {
@@ -13,32 +13,61 @@ export default class CartIcon {
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
+      this.elem.classList.add("cart-icon_visible");
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
         </div>`;
 
       this.updatePosition();
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend",
+        () => {
+          this.elem.classList.remove("shake");
+        },
+        { once: true }
+      );
     } else {
-      this.elem.classList.remove('cart-icon_visible');
+      this.elem.classList.remove("cart-icon_visible");
     }
   }
 
   addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
   }
 
   updatePosition() {
-    // ваш код ...
+    if (this.elem.offsetHeight !== 0 && document.body.clientWidth >= 767) {
+      if (document.body.getBoundingClientRect().top < 63) {
+        let leftCartPosition =
+          Math.min(
+            document.querySelector(".container").getBoundingClientRect().right +
+              20,
+            document.documentElement.clientWidth - this.elem.offsetWidth - 10
+          ) + "px";
+
+        Object.assign(this.elem.style, {
+          position: "fixed",
+          top: "50px",
+          zIndex: 1e3,
+          right: "10px",
+          left: leftCartPosition,
+        });
+      } else {
+        Object.assign(this.elem.style, {
+          position: "",
+          top: "",
+          left: "",
+          zIndex: "",
+        });
+      }
+    }
   }
 }
